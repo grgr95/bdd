7961f39f5284197024df78c0fbf95a114cc76af2
Tatiana Smirnova <redkiivid@mail.ru>
2023-06-02 08:52:20 +0300

    add gradle-wapper+upgade datahelper

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -8,7 +8,7 @@ import java.util.Locale;
 import java.util.NoSuchElementException;
 import java.util.Random;
 
-@Value
+
 public class DataHelper {
 
     private static Faker fake = new Faker(new Locale("ru"));
